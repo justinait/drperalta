4e6ddc0524a85c7dcfd88767f95e2feee47bdca8
justinait <justiiturraspe@gmail.com>
2024-05-28 20:14:42 -0300

    tablet

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,27 +7,30 @@ import ContactForm from './ContactForm'
 function Contact() {
   return (
     <div className='contactPageContainer' id='contact'>
-      <p className='contactHeroTitle'>Home medical care just a click away. <strong>Schedule your visit today.</strong> </p>
+      <div className='contactPageOne'>
 
-      <div className='divIcons'>
-        <img src={phone} alt="" />
-        <p>+52 3221303534</p>
+        <p className='contactHeroTitle'>Home medical care just a click away. <strong>Schedule your visit today.</strong> </p>
+
+        <div className='divIcons'>
+          <img src={phone} alt="" />
+          <p>+52 3221303534</p>
+        </div>
+
+        <div className='divIcons'>  
+          <img src={location} alt="" />
+          <p>City of Mexico</p>
+        </div>
+
+        <p className='subtitleContactHero'>At home medical service 24/7 to:</p>
+        <ul>
+          <li>Mismaloya</li>
+          <li>Puerto Vallarta</li>
+          <li>Nuevo Vallarta</li>
+          <li>Bucerias</li>
+          <li>La Cruz</li>
+          <li>Sayulita</li>
+        </ul>
       </div>
-
-      <div className='divIcons'>  
-        <img src={location} alt="" />
-        <p>City of Mexico</p>
-      </div>
-
-      <p className='subtitleContactHero'>At home medical service 24/7 to:</p>
-      <ul>
-        <li>Mismaloya</li>
-        <li>Puerto Vallarta</li>
-        <li>Nuevo Vallarta</li>
-        <li>Bucerias</li>
-        <li>La Cruz</li>
-        <li>Sayulita</li>
-      </ul>
 
       <ContactForm/>
     </div>
