2238e90235664197473e5e2ce518d386756c4e1a
justinait <justiiturraspe@gmail.com>
2024-05-28 10:58:15 -0300

    home

--- src/Components/Home/WhatWeDo.jsx
+++ src/Components/Home/WhatWeDo.jsx
@@ -3,15 +3,24 @@ import './WhatWeDo.css'
 
 function WhatWeDo() {
     const services = [
-        {image: ''}
+      {image: '/service1.jpg', name: 'Medical consultation'},
+      {image: '/service2.jpg', name: 'Laboratory'},
+      {image: '/service3.jpg', name: 'Pharmacy'},
+      {image: '/service4.jpg', name: 'Vaccines'},
+      {image: '/service5.jpg', name: 'XRay'},
+      {image: '/service6.jpg', name: 'Nursing'},
     ]
   return (
     <div className='whatContainer'>
-        <p className='whatWeDotitle'>What we do</p>
-        <div>
-            <img src="" alt="" />
-            <p></p>
-        </div>
+      <p className='whatWeDotitle'>What we do</p>
+      {
+        services.map((e, i)=>{
+          return <div key={i} className='whatItem'>
+            <img src={e.image} alt="" className='whatImage' />
+            <p className='whatName'>{e.name}</p>
+          </div>
+        })
+      }
     </div>
   )
 }
