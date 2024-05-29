ab61beb650109fca35369c2182e46cd9c571c59b
justinait <justiiturraspe@gmail.com>
2024-05-28 21:23:57 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,11 +5,13 @@ function Footer({scrollToSection}) {
   return (
     <div className='footer'>
       <p className='medicalCityFooter'>Medical City</p>
+      
+      <div>
+        <p className='indice' onClick={() => scrollToSection('what-we-do')}>What we do</p>
+        <p className='indice' onClick={() => scrollToSection('contact')}>Contact us</p>
+      </div>
 
-      <p className='indice' onClick={() => scrollToSection('what-we-do')}>What we do</p>
-      <p className='indice' onClick={() => scrollToSection('contact')}>Contact us</p>
-
-      <p className='copyright'>Copyright © 2024 Medical City PV
+      <p className='copyright'>Copyright © 2024 Medical City PV <br />
       WhatsApp 322-130-3534 - All Rights Reserved.</p>
     </div>
   )
